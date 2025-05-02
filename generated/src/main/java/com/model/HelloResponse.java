2cd206fba2bf8963d71f95e05d4a3259494801b3
eika <skdc0106@gmail.com>
2025-05-02 14:30:05 +0900

    初期コミット

---
+++ generated/src/main/java/com/model/HelloResponse.java
@@ -0,0 +1,94 @@
+package com.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * HelloResponse
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-04-30T15:14:50.384946800+09:00[Asia/Tokyo]", comments = "Generator version: 7.4.0")
+public class HelloResponse {
+
+  private String message;
+
+  public HelloResponse() {
+    super();
+  }
+
+  /**
+   * Constructor with only required parameters
+   */
+  public HelloResponse(String message) {
+    this.message = message;
+  }
+
+  public HelloResponse message(String message) {
+    this.message = message;
+    return this;
+  }
+
+  /**
+   * Get message
+   * @return message
+  */
+  @NotNull 
+  @Schema(name = "message", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("message")
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    HelloResponse helloResponse = (HelloResponse) o;
+    return Objects.equals(this.message, helloResponse.message);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(message);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class HelloResponse {\n");
+    sb.append("    message: ").append(toIndentedString(message)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
