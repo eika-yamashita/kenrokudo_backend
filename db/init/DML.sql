dd10f0caa0d344bb52d4e06fa8dec0f2f81689f2
Eika <65638670+eika-yamashita@users.noreply.github.com>
2025-12-31 20:27:07 +0900

    Update individual schema and seed data

--- db/init/DML.sql
+++ db/init/DML.sql
@@ -1,4 +1,6 @@
-INSERT INTO individual (name)
+INSERT INTO individual (species_cd, id, name, gender)
 VALUES
-  ('test-1'),
-  ('test-2');
+  ('0001', '25A001', 'AAAA', '0'),
+  ('0002', '25A002', 'BBBB', '1'),
+  ('0003', '25A003', 'CCCC', '0'),
+  ('0004', '25A004', 'DDDD', '1');
