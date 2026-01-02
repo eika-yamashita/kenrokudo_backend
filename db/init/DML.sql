33fe6a3123b5880d550627d643bd3aaaf3e86348
eika <skdc0106@gmail.com>
2026-01-02 10:20:00 +0900

    application.properties 修正

--- db/init/DML.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO individual (species_cd, id, name, gender)
-VALUES
-  ('0001', '25A001', 'Tangerine', '0'),
-  ('0001', '25A002', 'Tangerine', '1'),
-  ('0001', '25A003', 'Tremper', '0'),
-  ('0001', '25A004', 'Tremper', '1'),
-  ('0002', '25A001', 'Normal', '0'),
-  ('0002', '25A002', 'Amel', '1');
